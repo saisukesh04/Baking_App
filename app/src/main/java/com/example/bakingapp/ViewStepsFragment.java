fae82b5ead77bb13cec18918e1a6c3acb7e6aaf1
Sai Sukesh <sukesh.sai04@gmail.com>
2020-04-29 17:17:27 +0530

    Fully working free

--- app/src/main/java/com/example/bakingapp/ViewStepsFragment.java
+++ app/src/main/java/com/example/bakingapp/ViewStepsFragment.java
@@ -7,7 +7,6 @@ import androidx.fragment.app.FragmentManager;
 import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
 
-import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
